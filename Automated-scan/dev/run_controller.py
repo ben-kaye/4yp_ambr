832db7a85edc92f2bfdd8e6a076538c3332d58d1
Ben <benkaye001@gmail.com>
2022-03-24 16:09:24 +0000

    Update run_controller.py

--- Automated-scan/dev/run_controller.py
+++ Automated-scan/dev/run_controller.py
@@ -5,7 +5,7 @@ from Detect import Well_Detector
 # WD = Well_Detector()
 # Well_Detector.store_well_loc(WD.return_wells('./Automated-scan/dev/scan_19.bmp'))
 
-CT = Controller(start_index=6,out_folder='Experiment-processed', data_folder='Experiment-data')
+CT = Controller(start_index=1410,out_folder='../Exp-22-03-ANALYSIS', data_folder='../Exp-22-03')
 CT.recover_wells()
 
 CT.run_control(overwrite=True)  
