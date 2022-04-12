da27fd68a66b5c6aad67af0c6d6bf4176fb9db0d
ben-kaye <benkaye001@gmail.com>
2022-04-12 10:01:55 +0100

    update controller

--- Automated-scan/dev/run_offline_process.py
+++ Automated-scan/dev/run_offline_process.py
@@ -5,7 +5,6 @@ from Detect import Well_Detector
 # WD = Well_Detector()
 # Well_Detector.store_well_loc(WD.return_wells('./Automated-scan/dev/scan_19.bmp'))
 
-CT = Controller(start_index=0,out_folder='../Unit tests/TEST', data_folder='../Unit tests/Exp-03-14')
-CT.recover_wells()
+CT = Controller(start_index=0,out_folder='../Unit tests/Exp-22-03-ANALYSIS', alt_out_folder='../Unit tests/Exp-22-03-ANALYSIS2')
 
-CT.run_control(overwrite=True)  
+CT.recover_cropped() 
