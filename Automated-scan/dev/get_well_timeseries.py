f006933901ec3a58763ab24262ce08386604613a
ben-kaye <benkaye001@gmail.com>
2022-04-22 10:32:35 +0100

    make util

---
+++ Automated-scan/dev/get_well_timeseries.py
@@ -0,0 +1,28 @@
+import cv2
+from cv2 import ROTATE_90_CLOCKWISE
+import numpy as np
+
+# loc:
+path = './../Unit tests/Exp-12-04-ALS2/ims/wells_'
+out_path = './../Unit tests/out.bmp'
+
+final_index = 1239
+num = 10
+
+mult = round(final_index/num)
+
+indices = tuple(i*mult for i in range(num))
+
+ims = []
+
+for i in indices:
+    link = path + str(i) + '.png'
+
+    im = cv2.imread(link)
+    ims.append(cv2.rotate(im,ROTATE_90_CLOCKWISE))
+
+out_im = np.hstack(ims)
+cv2.imshow(None, out_im)
+cv2.waitKey(0)
+
+cv2.imwrite(out_path,out_im)
